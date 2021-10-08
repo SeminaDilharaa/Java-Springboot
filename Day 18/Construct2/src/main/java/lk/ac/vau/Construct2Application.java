701cc98ebdb60bb05cb79e8bbdfd60e2c031f143
Semina Dilhara <45710872+SeminiDilhara@users.noreply.github.com>
2021-10-08 19:23:56 +0530

    Add files via upload

---
+++ Day 18/Construct2/src/main/java/lk/ac/vau/Construct2Application.java
@@ -0,0 +1,13 @@
+package lk.ac.vau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Construct2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Construct2Application.class, args);
+	}
+
+}
